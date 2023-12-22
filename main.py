3d77a58d5b036c6856a5937c11732b285f2b6ac7
CI9654 <stephenchen@careerintlinc.com>
2023-12-22 18:17:31 +0800

    fdsaf

--- main.py
+++ main.py
@@ -9,5 +9,6 @@
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('PyCharm')
+    print("one")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
